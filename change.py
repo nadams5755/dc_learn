6597a3c9ae04eb1f26783b0d450e37f2c307d8b0
Nick Adams <nadams@agitated.net>
2015-04-01 00:13:57 -0700

    finished change math.  need to finish output.

--- change.py
+++ change.py
@@ -1,43 +1,83 @@
 import random
+import string
 
 my_bill_dollas = random.randint(0,101)
 my_bill_cents = random.randint(0,100)
 
-my_bill = float(str(my_bill_dollas) + "." + str(my_bill_cents))
+my_bill_dollas = 63
+my_bill_cents = 44
+
+# insert a 0 in the 10s place of the bill if the change is below $0.10.  ex: $0.04
+padding = ""
+if my_bill_cents < 10:
+  padding = "0"
+
+my_bill = float(str(my_bill_dollas) + "." + padding + str(my_bill_cents))
 
 print "your bill is $" + str(my_bill)
 
-change_d = { '100': 0, '50': 0, '20': 0, '10': 0, '5': 0, '1': 0 }
-change_c = { '25': 0, '10': 0, '5': 0, '1': 0}
+cash_input = str(float(raw_input("how much cash wouldy you like to pay with? ")))
 
-my_remain_bill = my_bill_dollas
+if float(cash_input) < my_bill:
+  print "eat shit and wash dishes!"
+elif my_bill == float(cash_input):
+  print "no change.  get lost shitlord."
+else:
+  cash = cash_input.split('.')
 
-# integer math, the bill is over $100
-if my_remain_bill / 100 != 0:
-  change_d['100'] = my_remain_bill / 100 # count bills needed
-  my_remain_bill -= change_d['100'] * 100 # update running total
-if my_remain_bill / 100 == 0 and my_remain_bill % 100 != 0:
-  if my_remain_bill / 50 != 0:
-    change_d['50'] = my_remain_bill / 50
-    my_remain_bill -= change_d['50'] * 50
-if my_remain_bill / 50 == 0 and my_remain_bill % 50 != 0:
-  if my_remain_bill / 20 != 0:
-    change_d['20'] = my_remain_bill / 20
-    my_remain_bill -= change_d['20'] * 20
-if my_remain_bill / 20 == 0 and my_remain_bill % 20 != 0:
-  if my_remain_bill / 10 != 0:
-    change_d['10'] = my_remain_bill / 10
-    my_remain_bill -= change_d['10'] * 10
-if my_remain_bill / 10 == 0 and my_remain_bill % 10 != 0:
-  if my_remain_bill / 5 != 0:
-    change_d['5'] = my_remain_bill / 5
-    my_remain_bill -= change_d['5'] * 5
-if my_remain_bill / 5 == 0 and my_remain_bill % 5 != 0:
-  if my_remain_bill / 1 != 0:
-    change_d['1'] = my_remain_bill / 1
-    my_remain_bill -= change_d['1'] * 1
+  my_cash_dollas = int(cash[0])
+  my_cash_cents = int(cash[1])
 
-print str(change_d)
-print str(my_remain_bill) + " remain"
+  # cents borrows a dollar if cents are less than the bill's cents.
+  if my_cash_cents <  my_bill_cents:
+    my_cash_dollas -= 1
+    my_cash_cents += 100
 
+  change_d = { '100': 0, '50': 0, '20': 0, '10': 0, '5': 0, '1': 0 }
+  change_c = { '25': 0, '10': 0, '5': 0, '1': 0}
 
+  my_remain_bill = my_cash_dollas - my_bill_dollas
+  # integer math, the bill is over $100
+  if my_remain_bill / 100 != 0:
+    change_d['100'] = my_remain_bill / 100 # count bills needed
+    my_remain_bill -= change_d['100'] * 100 # update running total
+  if my_remain_bill / 100 == 0 and my_remain_bill % 100 != 0:
+    if my_remain_bill / 50 != 0:
+      change_d['50'] = my_remain_bill / 50
+      my_remain_bill -= change_d['50'] * 50
+  if my_remain_bill / 50 == 0 and my_remain_bill % 50 != 0:
+    if my_remain_bill / 20 != 0:
+      change_d['20'] = my_remain_bill / 20
+      my_remain_bill -= change_d['20'] * 20
+  if my_remain_bill / 20 == 0 and my_remain_bill % 20 != 0:
+    if my_remain_bill / 10 != 0:
+      change_d['10'] = my_remain_bill / 10
+      my_remain_bill -= change_d['10'] * 10
+  if my_remain_bill / 10 == 0 and my_remain_bill % 10 != 0:
+    if my_remain_bill / 5 != 0:
+      change_d['5'] = my_remain_bill / 5
+      my_remain_bill -= change_d['5'] * 5
+  if my_remain_bill / 5 == 0 and my_remain_bill % 5 != 0:
+    if my_remain_bill / 1 != 0:
+      change_d['1'] = my_remain_bill / 1
+      my_remain_bill -= change_d['1'] * 1
+  print str(change_d)
+
+  my_remain_coin = my_cash_cents - my_bill_cents
+
+  if my_remain_coin / 25 != 0:
+      change_c['25'] = my_remain_coin / 25
+      my_remain_coin -= change_c['25'] * 25
+  if my_remain_coin / 25 == 0 and my_remain_coin % 25 != 0:
+    if my_remain_coin / 10 != 0:
+      change_c['10'] = my_remain_coin / 10
+      my_remain_coin -= change_c['10'] * 10
+  if my_remain_coin / 10 == 0 and my_remain_coin % 10 != 0:
+    if my_remain_coin / 5 != 0:
+      change_c['5'] = my_remain_coin / 5
+      my_remain_coin -= change_c['5'] * 5
+  if my_remain_coin / 5 == 0 and my_remain_coin % 5 != 0:
+    if my_remain_coin / 1 != 0:
+      change_c['1'] = my_remain_coin / 1
+      my_remain_coin -= change_c['1'] * 1
+  print str(change_c)
