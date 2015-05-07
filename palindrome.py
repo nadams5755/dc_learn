388c5661bee4e5716f4c7762442569f540ab42eb
Nick Adams <nadams@apple.com>
2015-05-07 09:46:41 -0700

    fogot that palindromes can be 3 letters.  ex: asa

--- palindrome.py
+++ palindrome.py
@@ -1,31 +1,22 @@
-mytest = str(raw_input("test for palindrome:"));
+mytest = str(raw_input("test for palindrome: "));
 
-if (len(mytest) % 2) == 0: 
-# has a chance to be a palindrome
-# because even number letters
-  print "even number of letters, that's a start."
+peer_char = len(mytest) - 1
+palindrome = False
 
-  peer_char = len(mytest) - 1
-  palindrome = False
-
-  for ch in mytest:
-    if peer_char >= (len(mytest) + 1)/2:
-      if ch == mytest[peer_char]:
-        print "letters match " + ch + ":" + mytest[peer_char]
-        peer_char -= 1
-        palindrome = True
-      else:
-        print "letters don't match " + ch + ":" + mytest[peer_char]
-        palindrome = False
-        break
+for ch in mytest:
+  if peer_char >= (len(mytest))/2:
+    if ch == mytest[peer_char]:
+      print "letters match " + ch + ":" + mytest[peer_char]
+      peer_char -= 1
+      palindrome = True
     else:
+      print "letters don't match " + ch + ":" + mytest[peer_char]
+      palindrome = False
       break
-
-  if palindrome == True:
-    print "it's a palindrome."
   else:
-    print "not a palindrome."
-    
+    break
 
+if palindrome == True:
+  print "it's a palindrome."
 else:
-  print "odd number of letters, no chance to be a palindrome."
+  print "not a palindrome."
